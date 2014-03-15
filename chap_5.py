a566ab0c9e8eceee1a76578705998c9b49feccb6
Andrew Schechtman-Rook <rook166@gmail.com>
2014-03-15 11:45:40 -0500

    Showing what happens if Equation 5.17 has -i*lambda_1

--- chap_5.py
+++ chap_5.py
@@ -16,14 +16,14 @@ def max_entropy_dice_priors(nsides = 6, mu=3.5):
 
 def compute_lambda1(lambda1,nsides,mu):
     ivals = np.linspace(1.,nsides,nsides)
-    expvals = np.exp(ivals*lambda1)
+    expvals = np.exp(-ivals*lambda1)
     return mu - np.sum(ivals*expvals)/np.sum(expvals)
 def compute_lambda0(lambda0,lambda1,nsides,mu):
     ivals = np.linspace(1.,nsides,nsides)
-    expvals = np.exp(ivals*lambda1)
-    return 1 - np.sum(expvals)*np.exp(-1+lambda0)/float(nsides)
+    expvals = np.exp(-ivals*lambda1)
+    return 1 - np.sum(expvals)*np.exp(-1-lambda0)/float(nsides)
 
 
 if __name__ == "__main__":
     max_entropy_dice_priors(nsides=6,mu=3.5)
-    max_entropy_dice_priors(nsides=6,mu=3.75)
+    max_entropy_dice_priors(nsides=6,mu=3.25)
